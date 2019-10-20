a65d902619db920b586a5b0c74f33ca636accb22
Eric Fischer <efischer0119@gmail.com>
2019-10-20 18:06:52 -0400

    LICENSE excluded from publish

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 aws s3 rm s3://www.whassat.info --recursive
-aws s3 sync . s3://www.whassat.info --exclude ".git*" --exclude "*.md" --exclude "*publish.sh" || exit 1
+aws s3 sync . s3://www.whassat.info --exclude ".git*" --exclude "*.md" --exclude "*publish.sh" --exclude "LICENSE" || exit 1
 for id in `cat ~/whassat.ids`; do
   aws cloudfront create-invalidation --distribution-id $id --paths "/*";
 done
